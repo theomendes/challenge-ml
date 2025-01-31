ddaa42a188b213ede3e9a715c063706ecd8b5fc6
Theo Mendes <theoafmendes@gmail.com>
2025-01-31 19:04:54 -0300

    ✅ - Added test for Request Type default values

--- Unit Tests/Network/Types/RequestTypeTests.swift
+++ Unit Tests/Network/Types/RequestTypeTests.swift
@@ -12,6 +12,13 @@ import Testing
 
 @Suite("Request Type Tests")
 struct RequestTypeTests {
+    @Test("Testing default values")
+    func testingDefaultValues() async throws {
+        let mock = DefaultValuesRequestTypeMock()
+        #expect(mock.method == .get)
+        #expect(mock.headers["Content-Type"] == "application/json")
+    }
+
     @Test("Testing request without parameters")
     func testingPlainParameters() async throws {
         let mock = RequestTypeMock(parameters: .plain)
@@ -23,6 +30,22 @@ struct RequestTypeTests {
         let requestParameters = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems
 
         #expect(requestParameters == nil)
+    }
+
+    @Test("Testing request with parameters")
+    func testingQueryParameters() async throws {
+        let parameters = [
+            "site_id": "MLB",
+        ]
+        let mock = RequestTypeMock(parameters: .parameters(parameters: parameters, encoding: URLEncoding.queryString))
+        let urlRequest = try mock.asURLRequest()
+        guard let url = urlRequest.url else {
+            Issue.record("URL Request should not be nil")
+            return
+        }
+        let requestParameters = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems
+
+        #expect(requestParameters?.first?.value == "MLB")
     }
 
     @Test("Testing invalid path")
