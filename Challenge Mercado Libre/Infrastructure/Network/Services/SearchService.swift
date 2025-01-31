15953f5bac1ef3407fd23646e41e6bac3c2274d7
Theo Mendes <theoafmendes@gmail.com>
2025-01-31 19:58:07 -0300

    🚧 - Created Search Service

---
+++ Challenge Mercado Libre/Infrastructure/Network/Services/SearchService.swift
@@ -0,0 +1,27 @@
+//
+//  SearchService.swift
+//  Challenge Mercado Libre
+//
+//  Created by Theo Mendes on 31/01/25.
+//
+
+import Alamofire
+import Foundation
+
+protocol SearchServiceType: ServiceType {
+    func query(_ q: String, category: String?, limit: Int, offset: Int, cache: CachedResponseHandler) async -> DataResponse<SearchResponse, AFError>
+}
+
+final class SearchService: SearchServiceType {
+    var session: Session
+
+    init(session: Session) {
+        self.session = session
+    }
+
+    func query(_ q: String, category: String?, limit: Int, offset: Int, cache: CachedResponseHandler = .cache) async -> DataResponse<SearchResponse, AFError> {
+        let api = SearchAPI(query: q, category: category, limit: limit, offset: offset)
+
+        return await request(api, cache: cache)
+    }
+}
