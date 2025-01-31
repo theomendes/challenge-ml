1f7cbdf065f0ad625d27741d927e43282fcd6788
Theo Mendes <theoafmendes@gmail.com>
2025-01-31 20:01:00 -0300

    🚧 - Created Mocked Data

---
+++ Unit Tests/Mocks/MockedData.swift
@@ -0,0 +1,17 @@
+//
+//  MockedData.swift
+//  Unit Tests
+//
+//  Created by Theo Mendes on 31/01/25.
+//
+
+import Foundation
+
+enum MockedData {
+    static let searchService: URL = {
+        guard let url = Bundle(for: MockSession.self).url(forResource: "SearchServiceResponse", withExtension: "json") else {
+            fatalError("Missing file: SearchServiceResponse.json")
+        }
+        return url
+    }()
+}
