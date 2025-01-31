58f69408670e7a278e3a17b79950b1ceade08fde
Theo Mendes <theoafmendes@gmail.com>
2025-01-31 17:15:37 -0300

    ✨ - Created initial project

---
+++ Challenge Mercado Libre/Challenge_Mercado_LibreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_Mercado_LibreApp.swift
+//  Challenge Mercado Libre
+//
+//  Created by Theo Mendes on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_Mercado_LibreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
