a62175cc7083bd29d56bf09aa5db76f4eebaffde
Theo Mendes <theoafmendes@gmail.com>
2025-02-01 12:42:31 -0300

    🚧 - Added logic to convert the response into entity

--- Challenge Mercado Libre/Domain/Use Cases/SearchUseCase.swift
+++ Challenge Mercado Libre/Domain/Use Cases/SearchUseCase.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol SearchUseCaseType {
-    func execute(for query: String, on siteID: String, category: String?, limit: Int, offset: Int) async throws -> SearchResponse
+    func execute(for query: String, on siteID: String, category: String?, limit: Int, offset: Int) async throws -> [SearchSection]
 }
 
 final class SearchUseCase: SearchUseCaseType {
@@ -18,14 +18,25 @@ final class SearchUseCase: SearchUseCaseType {
         self.repository = repository
     }
 
-    func execute(for query: String, on siteID: String, category: String?, limit: Int, offset: Int) async throws -> SearchResponse {
+    func execute(for query: String, on siteID: String, category: String?, limit: Int, offset: Int) async throws -> [SearchSection] {
         let response = await repository.getResults(query, siteID: siteID, category: category, limit: limit, offset: offset)
 
         switch response.result {
-        case .success(let value):
-            return value
+        case .success(let result):
+            return convertToSections(result)
         case .failure(let error):
             throw error
         }
+    }
+}
+
+extension SearchUseCase {
+    private func convertToSections(_ response: SearchResponse) -> [SearchSection] {
+        let items = response.results.map { result in
+            SearchItem(id: result.id, title: result.title, thumbnail: result.thumbnail)
+        }
+        return [
+            SearchSection(items: items)
+        ]
     }
 }
