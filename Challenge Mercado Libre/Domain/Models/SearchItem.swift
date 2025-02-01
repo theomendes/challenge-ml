6d850a97720d54507af506daf8f8f1ec5d5e0057
Theo Mendes <theoafmendes@gmail.com>
2025-02-01 11:14:28 -0300

    🚧 - Created Search entities

---
+++ Challenge Mercado Libre/Domain/Models/SearchItem.swift
@@ -0,0 +1,14 @@
+//
+//  SearchItem.swift
+//  Challenge Mercado Libre
+//
+//  Created by Theo Mendes on 01/02/25.
+//
+
+import Foundation
+
+struct SearchItem: Identifiable, Hashable {
+    let id: String
+    let title: String
+    let thumbnail: String
+}
---
+++ Challenge Mercado Libre/Domain/Models/SearchSection.swift
@@ -0,0 +1,12 @@
+//
+//  SearchSection.swift
+//  Challenge Mercado Libre
+//
+//  Created by Theo Mendes on 01/02/25.
+//
+
+import Foundation
+
+struct SearchSection: Hashable {
+    let items: [SearchItem]
+}
