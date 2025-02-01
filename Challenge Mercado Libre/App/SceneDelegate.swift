f9b65dd7b8cec48cdcccd9cdff70d50b6b011c21
Theo Mendes <theoafmendes@gmail.com>
2025-01-31 23:08:19 -0300

    🚧 - Created App and Scene Delegate

---
+++ Challenge Mercado Libre/App/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  Challenge Mercado Libre
+//
+//  Created by Theo Mendes on 31/01/25.
+//
+
+import UIKit
+import SwiftUI
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        let contentView = ContentView()
+
+        window.rootViewController = UIHostingController(rootView: contentView)
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
