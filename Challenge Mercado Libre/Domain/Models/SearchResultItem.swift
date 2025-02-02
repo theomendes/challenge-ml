e6fba878eb2c28343f0062a348e2b9aa4c494d56
Theo Mendes <theoafmendes@gmail.com>
2025-02-02 09:38:06 -0300

    🚧 - Added Permalink and mock

--- Challenge Mercado Libre/Domain/Models/SearchResultItem.swift
+++ Challenge Mercado Libre/Domain/Models/SearchResultItem.swift
@@ -14,4 +14,28 @@ struct SearchResultItem: Identifiable, Hashable {
     let price: SearchItemPrice
     let freeShipping: Bool
     let officialStore: String?
+    let permalink: URL
+}
+
+extension SearchResultItem {
+    static var mock: SearchResultItem {
+        .init(
+            id: "MLB5097135428",
+            title: "Apple Watch Series 10",
+            thumbnail: URL(string: "http://http2.mlstatic.com/D_837437-MLA79083751040_092024-I.jpg")!,
+            price: .init(
+                amount: 5499,
+                originalAmount: 6799,
+                currency: "BRL",
+                formatedAmount: "R$ 5499",
+                installments: .init(
+                    quantity: 10,
+                    amount: 549.90,
+                    currency: "BRL",
+                    formatedAmount: "R$ 549,90")),
+            freeShipping: true,
+            officialStore: "Apple",
+            permalink: URL(string: "https://www.mercadolivre.com.br/apple-watch-se-gps-caixa-estelar-de-aluminio-40-mm-pulseira-esportiva-estelar-pm/p/MLB27366066#wid=MLB5256403412&sid=unknown")!
+        )
+    }
 }
